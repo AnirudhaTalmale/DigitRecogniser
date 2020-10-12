c355ed4c0cf72ced4cafe1a8217fcb09b19698de
AnirudhaTalmale <48641595+AnirudhaTalmale@users.noreply.github.com>
2020-10-12 23:44:00 +0530

    Update hand_digits_recognizer.py

--- hand_digits_recognizer.py
+++ hand_digits_recognizer.py
@@ -27,9 +27,10 @@ clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 
 acc = confusion_matrix(y_test, predictions)
-print(acc)
 
 def accuracy(confusion_matrix):
    diagonal_sum = confusion_matrix.trace()
    sum_of_all_elements = confusion_matrix.sum()
    return diagonal_sum / sum_of_all_elements
+
+print(accuracy(acc))
